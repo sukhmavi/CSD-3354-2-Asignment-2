6fac4dd1cc0158798af44c0d96f75ac93b9798dc
visualstudio <visualstudio@122-PC34>
2019-03-06 11:06:02 -0500

    final commit 726660 723153

--- DelegateExcercises/Program.cs
+++ DelegateExcercises/Program.cs
@@ -4,8 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// sukhpreet sinfh c0726660
+// kajalpreet kaur dandeyala c0732153
+//csd3354_2
+//assignment 2 
+// march 6,2019
 namespace DelegateAndEvents
 {
     public class DelegateExcercises
-  
+    {
+        public delegate int MyDelegate(int intValue);
+
+        public int Method1(int intMethod1)
+        {
+            return intMethod1 * 2;
+        }
+
+        public int Method2(int intMethod2)
+        {
+            return intMethod2 * 10;
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            int result1 = myDelegate(10);
+            System.Console.WriteLine(result1);
+            myDelegate = new MyDelegate(Method2);
+            int result2 = myDelegate(10);
+            System.Console.WriteLine(result2);
+        }
+    }
+
+
+}
+class program
+{
+    static void Main(string []args)
+
+    {
+
+    }
+}
 
