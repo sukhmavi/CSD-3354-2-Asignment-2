aadc8c418c77e908554beb0a14c1e460d98d971a
visualstudio <visualstudio@122-PC34>
2019-03-06 11:00:59 -0500

    lab 4 initial

--- DelegateExcercises/Program.cs
+++ DelegateExcercises/Program.cs
@@ -4,36 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DelegateExcercises
-{
-    public class DelegateExcercises
-    {
-        public delegate void MyDelegate();
-        void Method1()
-        {
-            Console.WriteLine("Method1");
-            Console.ReadLine();
-        }
-        public void Method2()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-
-
-        }
-
-
-    }
-}
 namespace DelegateAndEvents
 {
-    class program
-    {
-        static void Main(string[] args)
-        {
-            // TO DO: Call Method2 from the DelegateExcercises class
-        }
-    }
-}
-
+    public class DelegateExcercises
+  
 
