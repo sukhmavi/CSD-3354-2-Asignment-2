a0f45f1dbe30cdcc28660cccc17414fc6c430e9e
visualstudio <visualstudio@122-PC34>
2019-03-06 11:23:45 -0500

    initial commit

--- DelegateExcercises/Program.cs
+++ DelegateExcercises/Program.cs
@@ -15,25 +15,4 @@ namespace DelegateAndEvents
     {
         public static void Main()
         {
-            DelegateExercises delegateExercises = new DelegateExercises();
-            delegateExercises.Method3();
-            Console.ReadLine();
-        }
-
-    }
-    public delegate void MyDelegate();
-
-    public class DelegateExercises
-    {
-        void Method1()
-        {
-            System.Console.WriteLine("Method1");
-        }
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
-        }
-    }
-
-}
+           
