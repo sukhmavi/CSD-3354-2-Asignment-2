fbf7a0d9566414b5d73b4a97f1e55f1c368a4769
visualstudio <visualstudio@122-PC34>
2019-03-06 11:15:58 -0500

    initial commit

--- DelegateExcercises/Program.cs
+++ DelegateExcercises/Program.cs
@@ -11,39 +11,3 @@ using System.Threading.Tasks;
 // march 6,2019
 namespace DelegateAndEvents
 {
-    public class DelegateExcercises
-    {
-        public delegate int MyDelegate(int intValue);
-
-        public int Method1(int intMethod1)
-        {
-            return intMethod1 * 2;
-        }
-
-        public int Method2(int intMethod2)
-        {
-            return intMethod2 * 10;
-        }
-
-        public void Method3()
-        {
-            MyDelegate myDelegate = new MyDelegate(Method1);
-            int result1 = myDelegate(10);
-            System.Console.WriteLine(result1);
-            myDelegate = new MyDelegate(Method2);
-            int result2 = myDelegate(10);
-            System.Console.WriteLine(result2);
-        }
-    }
-
-
-}
-class program
-{
-    static void Main(string []args)
-
-    {
-
-    }
-}
-
